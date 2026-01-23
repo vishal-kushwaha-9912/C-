896dfd83cb67f3d1867699a81585f6430919b284
vishal-kushwaha-9912 <vishalk.ushwa9912@gmail.com>
2026-01-23 21:45:46 +0530

    Add index.html

--- DAY13/binarysearch.cpp
+++ DAY13/binarysearch.cpp
@@ -17,7 +17,20 @@ int main(){
     int index=-1;
 
 //find middle index value
-    int mids= start+(end-start)/2;
+
+
+
+// int mid = (start+end)/2; : In this formula mostly large number is loverflow .
+
+//May overflow when start and end are large.
+
+//always used this method for the mid find.
+int mids= start+(end-start)/2; // Prevents overflow by avoiding direct addition
+
+
+    // int mids= start+(end-start)/2;   //why we used this formula ?.
+    // ANSWER : because the sum of number not ever time store  in INT  the large value then we used this to short it .
+
 
     if (arr[ mids ]==target)
     {
